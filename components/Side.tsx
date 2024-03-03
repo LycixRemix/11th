2d7fccf3ddeefb30c5018d1b651ec96f8250a6c8
LycixRemix <chengyongliu@foxmail.com>
2024-03-03 16:25:34 +0800

    feat(docs): update image address

--- components/Side.tsx
+++ components/Side.tsx
@@ -15,18 +15,18 @@ export default function Side() {
       <Image src="/images/wallhaven-9dee1d.webp" fill className="absolute h-full w-full object-cover" alt="" priority />
       <div className="absolute top-0 h-full w-full bg-[url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAAECAYAAACp8Z5+AAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAKUlEQVQImU3IMREAIAgAwJfNkQCEsH8cijjpMf6vnXlQaIiJFx+omEBfmqIEZLe2jzcAAAAASUVORK5CYII=')] object-cover" />
       <div className="absolute bottom-2 left-0 right-0 text-center text-white md:bottom-1/4">
-        <h1 className="p-2 text-3xl md:p-4 md:text-4xl">青梅煮茶初试探</h1>
+        <h1 className="p-2 text-3xl md:p-4 md:text-4xl">LycixRemix</h1>
         <div className="flex justify-center">
-          <Link href="" className="mb-2 px-3 md:mb-4">
+          <Link href="https://space.bilibili.com/525626978" className="mb-2 px-3 md:mb-4">
             <SiBilibili style={IconStyle} />
           </Link>
-          <Link href="https://github.com/liucys" className="mb-2 px-3 md:mb-4">
+          <Link href="https://github.com/LycixRemix" className="mb-2 px-3 md:mb-4">
             <SiGithub style={IconStyle} />
           </Link>
           <Link href="mailto:chengyongliu@foxmail.com" className="mb-2 px-3 md:mb-4">
             <SiGmail style={IconStyle} />
           </Link>
-          <Link href="" className="mb-2 px-3 md:mb-4">
+          <Link href="https://twitter.com/ALinQuestion" className="mb-2 px-3 md:mb-4">
             <SiTwitter style={IconStyle} />
           </Link>
         </div>
